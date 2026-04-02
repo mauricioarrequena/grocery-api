d273a27b613da83e1e6a1e5f08fe2076351f24f9
mauricio arroyo assuresoft <mauricio.arroyo@assuresoft.com>
2026-04-01 20:22:17 -0400

    feat01: add new method

--- src/repositories/category.repository.ts
+++ src/repositories/category.repository.ts
@@ -2,7 +2,11 @@ import { AppDataSource } from "../config/data-source";
 import { Category } from "../entities/category.entity";
 
 export class CategoryRepository {
-  private categoryRepository = AppDataSource.getRepository(Category);
+  private readonly categoryRepository = AppDataSource.getRepository(Category);
+
+  async create(category: Category): Promise<Category> {
+    return await this.categoryRepository.save(category);
+  }
 
   async getAll(): Promise<Category[]> {
     return await this.categoryRepository.find();
